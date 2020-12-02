4c15dcfe9208307eba986da25631369133c3a976
Maggie Danger <magnus@ef.gy>
2020-12-02 05:51:29 +0000

    start splitting things into smaller, manageable pieces: this time, move the gen1 character map to its own header file, create the version file and do some minor cleanups - partly around typing (set is better than a vector for most of these)

---
+++ include/pokemon-randomiser/version.h
@@ -0,0 +1,18 @@
+/* Version information
+ *
+ * This header contains the library's version number and some additional
+ * documentation that didn't seem to fit in anywhere else.
+ */
+
+#if !defined(POKEMON_RANDOMISER_VERSION_H)
+#define POKEMON_RANDOMISER_VERSION_H
+
+namespace pokemon {
+namespace randomiser {
+/* Library version
+ */
+static const unsigned int version = 1;
+}  // namespace randomiser
+}  // namespace pokemon
+
+#endif
